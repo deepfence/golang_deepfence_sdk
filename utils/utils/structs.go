2d948885a5247fa6a3fee30fc43096889abcdc18
gnmahanth <mahanth@deepfence.io>
2023-01-23 17:26:20 +0000

    update structs

--- utils/utils/structs.go
+++ utils/utils/structs.go
@@ -6,16 +6,16 @@ type SbomRequest struct {
 }
 
 type SbomParameters struct {
-	ImageName             string `json:	"image_name,omitempty"`
-	ImageId               string `json:"image_id,o	mitempty"`
+	ImageName             string `json:"image_name,omitempty"`
+	ImageId               string `json:"image_id,omitempty"`
 	ScanId                string `json:"scan_id,omitempty"`
 	KubernetesClusterName string `json:"kubernetes_cluster_name,omitempty"`
 	HostName              string `json:"host_name,omitempty"`
 	NodeId                string `json:"node_id,omitempty"`
 	NodeType              string `json:"node_type,omitempty"`
-	ScanType              string `json:"sca	n_type,omitempty"`
-	ContainerName         string `json:"container_name,	omitempty"`
-	SBOMFilePath          string `json:"sbom_file_path,omitemp	ty"`
+	ScanType              string `json:"scan_type,omitempty"`
+	ContainerName         string `json:"container_name,omitempty"`
+	SBOMFilePath          string `json:"sbom_file_path,omitempty"`
 	Mode                  string `json:"mode,omitempty"`
 }
 
