99aa8a42369c2e26228d135c4358fd66f5a9ffbb
Harshvardhan Karn <harshvardhan@deepfence.io>
2023-02-10 10:56:06 +0000

    add secretscanner struct for registry scanning

--- utils/utils/structs.go
+++ utils/utils/structs.go
@@ -23,3 +23,17 @@ type SbomParameters struct {
 type SbomBody struct {
 	SBOM string `json:"sbom" required:"true"`
 }
+
+type SecretScanParameters struct {
+	ImageName             string `json:"image_name"`
+	ImageId               string `json:"image_id"`
+	ScanId                string `json:"scan_id" required:"true"`
+	KubernetesClusterName string `json:"kubernetes_cluster_name"`
+	HostName              string `json:"host_name"`
+	NodeId                string `json:"node_id"`
+	NodeType              string `json:"node_type"`
+	ScanType              string `json:"scan_type"`
+	ContainerName         string `json:"container_name"`
+	Mode                  string `json:"mode,omitempty"`
+	RegistryId            string `json:"registry_id,omitempty"`
+}
