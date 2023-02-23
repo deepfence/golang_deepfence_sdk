010832fb9cbf71a43b634e866d0a3eede0f9131e
Mukul Varshney <72725782+mukuldeepfence@users.noreply.github.com>
2023-02-24 01:02:04 +0530

    notification task added

--- utils/utils/constants.go
+++ utils/utils/constants.go
@@ -39,6 +39,7 @@ const (
 	ReportGeneratorTaskXLSX   = "tasks_generate_xlsx_report"
 	ReportGeneratorTaskPDF    = "tasks_generate_pdf_report"
 	ComputeThreatTask         = "compute_threat"
+	SendNotificationTask      = "tasks_send_notification"
 )
 
 const (
