a93b78679682164fc0fc54431f6a774b52da57ff
Thomas Legris <thomas@deepfence.io>
2023-03-13 11:48:15 +0900

    Make directory clients access thread safe

--- utils/directory/utils.go
+++ utils/directory/utils.go
@@ -2,6 +2,7 @@ package directory
 
 import (
 	"context"
+	"sync"
 
 	postgresqlDb "github.com/deepfence/golang_deepfence_sdk/utils/postgresql/postgresql-db"
 	"github.com/minio/minio-go/v7"
@@ -9,11 +10,38 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+var locks []sync.Mutex
+
+func init() {
+	locks = make([]sync.Mutex, 5)
+}
+
+func getLock[T any]() *sync.Mutex {
+	var val T
+	switch any(val).(type) {
+	case *redis.Client:
+		return &locks[1]
+	case *CypherDriver:
+		return &locks[2]
+	case *postgresqlDb.Queries:
+		return &locks[3]
+	case *minio.Client:
+		return &locks[4]
+	}
+	// Global fallback that should never be reached
+	return &locks[0]
+}
+
 func getClient[T *redis.Client | *CypherDriver | *postgresqlDb.Queries | *minio.Client](ctx context.Context, pool map[NamespaceID]T, newClient func(DBConfigs) (T, error)) (T, error) {
 	key, err := ExtractNamespace(ctx)
 	if err != nil {
 		return nil, err
 	}
+
+	lock := getLock[T]()
+	lock.Lock()
+	defer lock.Unlock()
+
 	val, has := pool[key]
 	if has {
 		return val, nil
