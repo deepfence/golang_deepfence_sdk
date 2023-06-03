7023d71353d4fbd34de2c0ddfc9756d567e94f02
Thomas Legris <thomas@deepfence.io>
2023-06-03 23:40:31 +0900

    Fix retry mechanism on refresh token

--- utils/http/client.go
+++ utils/http/client.go
@@ -220,20 +220,20 @@ func (cl *OpenapiHttpClient) refreshToken() error {
 
 	resp, http_resp, err := cl.refresher.AuthenticationAPI.AuthTokenRefreshExecute(req)
 	if err != nil {
-		return err
-	}
+		if http_resp != nil && http_resp.StatusCode == http.StatusUnauthorized && cl.api_token != nil {
+			req := cl.refresher.AuthenticationAPI.AuthToken(context.Background()).ModelApiAuthRequest(openapi.ModelApiAuthRequest{
+				ApiToken: *cl.api_token,
+			})
+			resp, http_resp, err = cl.refresher.AuthenticationAPI.AuthTokenExecute(req)
+			if err != nil {
+				return AuthError
+			}
 
-	if http_resp.StatusCode == http.StatusUnauthorized && cl.api_token != nil {
-		req := cl.refresher.AuthenticationAPI.AuthToken(context.Background()).ModelApiAuthRequest(openapi.ModelApiAuthRequest{
-			ApiToken: *cl.api_token,
-		})
-		resp, http_resp, err = cl.refresher.AuthenticationAPI.AuthTokenExecute(req)
-		if err != nil {
-			return AuthError
-		}
-
-		if http_resp.StatusCode == http.StatusUnauthorized {
-			return AuthError
+			if http_resp.StatusCode == http.StatusUnauthorized {
+				return AuthError
+			}
+		} else {
+			return err
 		}
 	}
 
